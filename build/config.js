c38c6d9c8c6aedbd74009eb596f99e6979368d98
Marco Massarotto <massa84@gmail.com>
2016-01-18 12:32:02 +0000

    [MM,Luis] fixing build process

--- build/config.js
+++ build/config.js
@@ -7,124 +7,104 @@ var knownOptions = {
   default: {env: process.env.NODE_ENV || 'development'}
 };
 
-var videojs4x = 'bower_components/video-4x.js/dist/video-js/';
-var videojs5x = 'bower_components/video-5x.js/dist/video-js/';
+var versionsMap = {
+  '4x' : 'bower_components/video-4x.js/dist/video-js/',
+  '5x' : 'bower_components/video-5x.js/dist/video-js/'
+}
+
 
 var options = parseArgs(process.argv.slice(2), knownOptions);
-module.exports = {
-  git:{
-    remoteUrl: process.env.GH_TOKEN ? "https://"+process.env.GH_TOKEN+"@github.com/MailOnline/videojs-vast-vpaid" : "origin"
-  },
-  options: options,
-  env: options.env,
-  //Files needed to build the demo
-  demo: {
-    pages: [
-      'demo/tpls/index.html'
-    ],
+module.exports = function(version) {
+  var videoJsBasePath = versionsMap[version];
+  if(!videoJsBasePath) {
+    throw new Error('invalid videojs version')
+  }
+  return {
+    git:{
+      remoteUrl: process.env.GH_TOKEN ? "https://"+process.env.GH_TOKEN+"@github.com/MailOnline/videojs-vast-vpaid" : "origin"
+    },
+    options: options,
+    env: options.env,
+    //Files needed to build the demo
+    demo: {
+      pages: [
+        'demo/tpls/index.html'
+      ],
 
-    styles:  {
-      4x: [
-        videojs4x + 'video-js.css',
+      styles: [
+        videoJsBasePath + 'video-js.css',
         'demo/styles/*.css'
       ],
-      5x: [
-        videojs5x + 'video-js.css',
-        'demo/styles/*.css'
+
+      assets: [
+        'demo/assets/*'
+      ],
+
+      fonts: [
+        videoJsBasePath + 'font/*'
+      ],
+
+      scripts: [
+        videoJsBasePath + 'video.dev.js'
       ]
     },
 
-    assets: [
-      'demo/assets/*'
-    ],
-
-    fonts: {
-      4x: [
-        videojs4x + 'font/*'
+    //Vendor files
+    vendor: {
+      scripts: [
+        'bower_components/VPAIDFLASHClient/bin/VPAIDFLASHClient.js',
+        'bower_components/VPAIDHTML5Client/bin/VPAIDHTML5Client.js'
       ],
-      5x: [
-        videojs5x + 'font/*'
+      //Whatever we add in here will be added to the build as a required resource
+      libraries: [
+        'node_modules/es5-shim/es5-shim.js', //Required for the player to work on old browsers
+        'lib/ie8fix.js', //Required for make the ad's work in ie8
+        'bower_components/swfobject/swfobject/src/swfobject.js'
+      ],
+      sourcemaps: [
+        'bower_components/VPAIDFLASHClient/bin/VPAIDFLASHClient.js.map'
+      ],
+      styles: [
+        //Empty for the moment
+      ],
+      assets: [
+        'bower_components/VPAIDFLASHClient/bin/VPAIDFlash.swf'
+      ],
+      fonts: [
+        //Empty for the moment
       ]
     },
 
-    scripts: {
-      4x: [
-        videojs4x + 'video.dev.js'
-      ],
-      5x: [
-        videojs5x + 'video.dev.js'
-      ]
-    }
-  },
-
-  //Vendor files
-  vendor: {
-    scripts: [
-      'bower_components/VPAIDFLASHClient/bin/VPAIDFLASHClient.js',
-      'bower_components/VPAIDHTML5Client/bin/VPAIDHTML5Client.js'
-    ],
-    //Whatever we add in here will be added to the build as a required resource
-    libraries: [
-      'node_modules/es5-shim/es5-shim.js', //Required for the player to work on old browsers
-      'lib/ie8fix.js', //Required for make the ad's work in ie8
-      'bower_components/swfobject/swfobject/src/swfobject.js'
-    ],
-    sourcemaps: [
-      'bower_components/VPAIDFLASHClient/bin/VPAIDFLASHClient.js.map'
-    ],
-    styles: [
-      //Empty for the moment
-    ],
-    assets: [
-      'bower_components/VPAIDFLASHClient/bin/VPAIDFlash.swf'
-    ],
-    fonts: [
-      //Empty for the moment
-    ]
-  },
-
-  plugin: {
-    scripts: {
-      4x: [
+    plugin: {
+      scripts: [
         'src/utils/pollyfill.js',
         'src/utils/utilityFunctions.js',
         'src/utils/**/*.js',
         'src/ads/**/*.js',
-        'src/plugin/videojs5x.vast.js',
+        'src/plugin/videojs4x.vast.js',
         'src/plugin/components/**/*.js'
       ],
-      5x: [
-        'src/utils/pollyfill.js',
-        'src/utils/utilityFunctions.js',
-        'src/utils/**/*.js',
-        'src/ads/**/*.js',
-        'src/plugin/videojs5x.vast.js',
-        'src/plugin/components/**/*.js'
-      ]
+      styles: [
+        'src/**/*.css'
+      ],
+      tests: {
+        unit: [
+          'test/test-utils.css',
+          'test/test-utils.js',
+          'test/**/*.js'
+        ]
+      }
     },
-    styles: [
-      'src/**/*.css'
-    ],
-    tests: {
-      unit: [
-        'test/test-utils.css',
-        'test/test-utils.js',
-        'test/**/*.js'
-      ]
-    }
-  },
 
-  //App files for production
-  prodfile: {
-    scripts: {
-      4x: pkg.name + '4x.js',
-      5x: pkg.name + '5x.js'
-    }
-    styles: pkg.name + '.css'
-  },
+    //App files for production
+    prodfile: {
+      scripts: pkg.name + '-' + version + '.js',
+      styles: pkg.name + '-' + version + '.css'
+    },
 
-  //Dist folder
-  DIST: path.normalize('__dirname/../bin'),
-  DEV: path.normalize('__dirname/../dev')
+    //Dist folder
+    DIST: path.normalize('__dirname/../bin'),
+    DEV: path.normalize('__dirname/../dev')
 
-};
+  };
+}
